3ba071c9ba96269d343a5350baeab025d0cac424
chuniee0126 <chunie0126@gmail.com>
2017-10-10 05:01:20 +0900

    create : header Wraping - Chunmyong.Park

---
+++ header_Chunmyong/DLOP.h
@@ -0,0 +1,22 @@
+#ifndef OPERATOR_SET_H
+#define OPERATOR_SET_H value
+
+// for basic Parent Class in DLOP
+#include "Ark.h"
+#include "NeuralNetwork.h"
+#include "Operator.h"
+#include "Objective.h"
+#include "Optimization.h"
+
+
+// Child Class of Operator
+#include "Convolution.h"
+
+// Child Class of Objective
+
+// Child Class of Optimization
+
+// Child Class of <NULL>
+
+
+#endif // OPERATOR_SET_H
