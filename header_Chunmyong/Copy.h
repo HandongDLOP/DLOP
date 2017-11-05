40a6afeb691a8e378d10efcb3aaa1815090c5684
chuniee0126 <chunie0126@gmail.com>
2017-11-06 07:13:22 +0900

    add : API test code - Chunmyong

---
+++ header_Chunmyong/Copy.h
@@ -0,0 +1,41 @@
+#ifndef COPY_H_
+#define COPY_H_    value
+
+#include <iostream>
+#include <string>
+
+#include "Tensor.h"
+#include "Operator.h"
+
+class Copy : public Operator {
+public:
+    Copy() : Operator() {
+        std::cout << "Copy::Copy()" << '\n';
+    }
+
+    // Constructor의 작업 순서는 다음과 같다.
+    // 상속을 받는 Operator(Parent class)의 Alloc()을 실행하고, (Operator::Alloc())
+    // 나머지 MetaParameter에 대한 Alloc()을 진행한다. (Copy::Alloc())
+    Copy(Operator *pInput, std::string pName) : Operator(pInput, pName) {
+        std::cout << "Copy::Copy(Operator *, MetaParameter *)" << '\n';
+        // Alloc(pInput, pParam);
+    }
+
+    virtual ~Copy() {
+        std::cout << "Copy::~Copy()" << '\n';
+    }
+
+    virtual bool ComputeForwardPropagate(){
+
+        std::cout << GetName() << " : ComputeForwardPropagate()" << '\n';
+
+        SetOutput(GetInput());
+
+        GetInput()->PrintData();
+
+        return true;
+    }
+
+};
+
+#endif  // COPY_H_
--- header_Chunmyong/NeuralNetwork_MLP.cpp
+++ header_Chunmyong/NeuralNetwork_MLP.cpp
@@ -5,11 +5,19 @@
 
 
 int main(int argc, char const *argv[]) {
-
     NeuralNetwork HGUNN;
 
-    Operator *x =  HGUNN.AddPlaceholder("x");
+    Tensor * var_1 = new Tensor(2, {5, 4}, TRUNCATED_NORMAL);
 
+    var_1->PrintData();
+
+    Operator *x = new Variable(var_1, "x");
+
+    Operator *copy_1 = new Copy(x, "copy_1");
+
+    Operator *copy_2 = new Copy(copy_1, "copy_2");
+
+    HGUNN.Training(x, copy_2);
 
 
     return 0;
