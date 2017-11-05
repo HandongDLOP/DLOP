fdd1919357a7fe28ec01e63d8f1e301a776c9a7d
chuniee0126 <chunie0126@gmail.com>
2017-11-06 03:24:26 +0900

    add : API test code - Chunmyong

---
+++ header_Chunmyong/NeuralNetwork_MLP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include "NeuralNetwork.h"
+
+
+int main(int argc, char const *argv[]) {
+
+    NeuralNetwork HGUNN;
+
+    Operator *x =  HGUNN.AddPlaceholder("x");
+
+
+
+    return 0;
+}
---
+++ header_Chunmyong/NeuralNetwork_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+#include "DLOP.h"
+
+int main(int argc, char const *argv[]) {
+    std::cout << "---------------Start-----------------" << '\n';
+
+    NeuralNetwork HGUNN;
+
+
+
+    std::cout << "---------------Training-----------------" << '\n';
+
+
+
+    std::cout << "---------------Testing-----------------" << '\n';
+
+
+    return 0;
+}
