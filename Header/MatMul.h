9bf221c5fff93ed23e3ca916d0c911f4ad096da0
master <chunie0126@gmail.com>
2017-11-22 01:54:49 +0900

    mod : problem with delta on Weight - Chunmyong

--- Header/MatMul.h
+++ Header/MatMul.h
@@ -9,6 +9,7 @@
 
 class MatMul : public Operator {
 public:
+
     // Constructor의 작업 순서는 다음과 같다.
     // 상속을 받는 Operator(Parent class)의 Alloc()을 실행하고, (Operator::Alloc())
     // 나머지 MetaParameter에 대한 Alloc()을 진행한다. (MatMul::Alloc())
@@ -28,6 +29,7 @@ public:
 
     virtual bool Alloc(Operator *pInput1, Operator *pInput2) {
         std::cout << "MatMul::Alloc(Operator *, Operator *)" << '\n';
+
         // if pInput1 and pInput2의 shape가 다르면 abort
 
         // Input dim 저장
@@ -74,15 +76,15 @@ public:
         int hidden = m_pInputDim0->Getdim()[1];
         int col    = m_pInputDim1->Getdim()[1];
 
-        float *data0  = GetInput()[0]->GetData();
-        float *data1  = GetInput()[1]->GetData();
-        float *result = new float[row * col];
-        float  temp   = 0.0;
+        float *input_data = GetInput()[0]->GetData();
+        float *Weight     = GetInput()[1]->GetData();
+        float *result     = new float[row * col];
+        float  temp       = 0.0;
 
         for (int row0 = 0; row0 < row; row0++) {
             for (int col1 = 0; col1 < col; col1++) {
                 for (int hid = 0; hid < hidden; hid++) {
-                    temp += data0[hidden * row0 + hid] * data1[col * hid + col1];
+                    temp += input_data[hidden * row0 + hid] * Weight[col * hid + col1];
                 }
                 result[col * row0 + col1] = temp;
                 temp                      = 0;
@@ -98,16 +100,16 @@ public:
         std::cout << GetName() << " : ComputeBackPropagate()" << '\n';
 
         // TensorShape *m_pInputDim0 = GetInputDim()[0];
-        TensorShape *m_pInputDim1 = GetInputDim()[1];
+        TensorShape *output_dim = GetInputDim()[1];
 
         // int row    = m_pInputDim0->Getdim()[0];
-        int hidden = m_pInputDim1->Getdim()[0];
+        int output_col = output_dim->Getdim()[1];
 
-        int size0 = GetInput()[0]->GetFlatDim();
-        int size1 = GetInput()[1]->GetFlatDim();
+        int size_input  = GetInput()[0]->GetFlatDim();
+        int size_Weight = GetInput()[1]->GetFlatDim();
 
-        float *data0 = GetInput()[0]->GetData();  // Weight
-        float *data1 = GetInput()[1]->GetData();  // input
+        float *input_data = GetInput()[0]->GetData(); // Weight
+        float *Weight     = GetInput()[1]->GetData(); // input
 
         GetInput()[0]->PrintData();
         GetInput()[1]->PrintData();
@@ -121,24 +123,33 @@ public:
 
         float *delta = GetDelta()->GetData();
 
-        float *_delta0 = new float[size0];  // for weight
-        float *_delta1 = new float[size1];  // for input
+        GetDelta()->PrintData();
 
-        //초기화 (나중에 코드 전체에 초기화 코드를 둘 것)
-        for (int i = 0; i < size1; i++){
-            _delta1[i] = 0;
+        float *_delta_input  = new float[size_input];  // for weight
+        float *_delta_Weight = new float[size_Weight]; // for input
+
+        // 초기화 (나중에 코드 전체에 초기화 코드를 둘 것)
+        for (int i = 0; i < size_input; i++) {
+            _delta_input[i] = 0;
         }
 
-        for (int i = 0; i < size0; i++) {
-            _delta0[i]           = delta[i / hidden] * data1[i % hidden];
-            _delta1[i % hidden] += delta[i / hidden] * data0[i];
+        for (int i = 0; i < size_Weight; i++) {
+            _delta_Weight[i] = 0;
         }
 
-        GetInputOperator()[0]->SetDelta(_delta0);
+        for (int i = 0; i < size_Weight; i++) {
+            _delta_input[i / output_col] += delta[i % output_col] * Weight[i];
+            _delta_Weight[i]              = delta[i % output_col] * input_data[i / output_col];
+
+            // _delta_input[i]           = delta[i / hidden] * Weight[i % hidden];
+            // _delta_Weight[i % hidden] += delta[i / hidden] * input_data[i];
+        }
+
+        GetInputOperator()[0]->SetDelta(_delta_input);
 
         GetInputOperator()[0]->GetDelta()->PrintData();
 
-        GetInputOperator()[1]->SetDelta(_delta1);
+        GetInputOperator()[1]->SetDelta(_delta_Weight);
 
         GetInputOperator()[1]->GetDelta()->PrintData();
 
@@ -146,4 +157,4 @@ public:
     }
 };
 
-#endif  // MATMUL_H_
+#endif // MATMUL_H_
