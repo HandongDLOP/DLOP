35769348e9b64a6a3e2b4b1b6cc50952bfa267c6
chuniee0126 <chunie0126@gmail.com>
2017-12-26 23:07:31 +0900

    mod : Operator - Chunmyong

--- Header/Operator.cpp
+++ Header/Operator.cpp
@@ -4,289 +4,276 @@ template class Operator<int>;
 template class Operator<float>;
 template class Operator<double>;
 
-template<typename DTYPE>
-int Operator<DTYPE>::Alloc(Tensor<DTYPE> *pTensor) {
+template<typename DTYPE> Operator<DTYPE>::Operator(std::string pName) {
+    std::cout << "Operator<DTYPE>::Operator()" << '\n';
+    m_aResult             = NULL;
+    m_aGradient           = NULL;
+    m_aDelta              = NULL;
+    m_apOutput            = NULL;
+    m_apInput             = NULL;
+    m_OutputDegree        = 0;
+    m_InputDegree         = 0;
+    m_currentOutputDegree = 0;
+    m_currentInputDegree  = 0;
+    m_name                = pName;
+}
+
+template<typename DTYPE> Operator<DTYPE>::Operator(Tensor<DTYPE> *pTensor, std::string pName) {
+    std::cout << "Operator<DTYPE>::Operator()" << '\n';
+    m_aResult             = NULL;
+    m_aGradient           = NULL;
+    m_aDelta              = NULL;
+    m_apOutput            = NULL;
+    m_apInput             = NULL;
+    m_OutputDegree        = 0;
+    m_InputDegree         = 0;
+    m_currentOutputDegree = 0;
+    m_currentInputDegree  = 0;
+    m_name                = pName;
+    Alloc(pTensor);
+}
+
+template<typename DTYPE> Operator<DTYPE>::Operator(Operator<DTYPE> *pInput, std::string pName) {
+    std::cout << "Operator<DTYPE>::Operator()" << '\n';
+    m_aResult             = NULL;
+    m_aGradient           = NULL;
+    m_aDelta              = NULL;
+    m_apOutput            = NULL;
+    m_apInput             = NULL;
+    m_OutputDegree        = 0;
+    m_InputDegree         = 0;
+    m_currentOutputDegree = 0;
+    m_currentInputDegree  = 0;
+    m_name                = pName;
+    Alloc(1, pInput);
+}
+
+template<typename DTYPE> Operator<DTYPE>::Operator(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1, std::string pName) {
+    std::cout << "Operator<DTYPE>::Operator()" << '\n';
+    m_aResult             = NULL;
+    m_aGradient           = NULL;
+    m_aDelta              = NULL;
+    m_apOutput            = NULL;
+    m_apInput             = NULL;
+    m_OutputDegree        = 0;
+    m_InputDegree         = 0;
+    m_currentOutputDegree = 0;
+    m_currentInputDegree  = 0;
+    m_name                = pName;
+    Alloc(2, pInput0, pInput1);
+}
+
+template<typename DTYPE> Operator<DTYPE>::~Operator() {
+    std::cout << "Operator<DTYPE>::~Operator()" << '\n';
+    this->Delete();
+}
+
+template<typename DTYPE> int Operator<DTYPE>::Alloc(Tensor<DTYPE> *pTensor) {
     std::cout << "Operator<DTYPE>::Alloc(Tensor<DTYPE> *)" << '\n';
 
-    return 1;
+    m_aResult = pTensor;
+
+    return TRUE;
 }
 
-template<typename DTYPE>
-int Operator<DTYPE>::Alloc(Operator<DTYPE> *pInput) {
-    std::cout << "Operator<DTYPE>::Alloc(Operator<DTYPE> *)" << '\n';
+template<typename DTYPE> int Operator<DTYPE>::Alloc(int numInput, ...) {
+    std::cout << "Operator<DTYPE>::Alloc(Tensor<DTYPE> *)" << '\n';
 
-    // Shape도 받을 수 있도록 코드 수정 alloc도 마찬가지
-    this->AddEdgebetweenOperators(pInput);
+    va_list ap;
+    va_start(ap, numInput);
 
-    return 1;
-}
-
-template<typename DTYPE>
-int Operator<DTYPE>::Alloc(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1) {
-    std::cout << "Operator<DTYPE>::Alloc(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
-
-    // Shape도 받을 수 있도록 코드 수정 alloc도 마찬가지
-    this->AddEdgebetweenOperators(pInput0);
-    this->AddEdgebetweenOperators(pInput1);
-
-    return 1;
-}
-
-template<typename DTYPE>
-int Operator<DTYPE>::Alloc(MetaParameter<DTYPE> *pParam) {
-    return 1;
-}
-
-// int Operator<DTYPE>::AllocOptimizer(Optimizer_name pOptimizer_name) {
-// for (int i = 0; i < m_InputDegree; i++) {
-// m_apInputOperator[i]->AllocOptimizer(pOptimizer_name);
-//
-// Optimizer<DTYPE> *pOptimizer = Factory::OptimizerFactory(pOptimizer_name);
-// m_apInputOperator[i]->SetOptimizer(pOptimizer);
-// }
-//
-// return 1;
-// }
-
-template<typename DTYPE>
-int Operator<DTYPE>::AllocOptimizer(Optimizer<DTYPE> *pOptimizer) {
-    for (int i = 0; i < m_InputDegree; i++) {
-        m_apInputOperator[i]->AllocOptimizer(pOptimizer);
-
-        if (m_apInputOperator[i]->GetTrainable() == 1) {
-            pOptimizer->AddTrainableData(m_apInputOperator[i]->GetOutput(), m_apInputOperator[i]->GetGradient());
-        }
+    for (int i = 0; i < numInput; i++) {
+        this->AddEdgebetweenOperators(va_arg(ap, Operator<DTYPE> *));
     }
 
-    return 1;
+    va_end(ap);
+
+    return TRUE;
 }
 
-template<typename DTYPE>
-void Operator<DTYPE>::Delete() {
-    std::cout << "Operator<DTYPE>::Delete()" << '\n';
+template<typename DTYPE> void Operator<DTYPE>::Delete() {
+    if (m_aResult) {
+        delete m_aResult;
+        m_aResult = NULL;
+    }
 
-    if (m_aOutput != NULL) delete m_aOutput;
+    if (m_aGradient) {
+        delete m_aGradient;
+        m_aGradient = NULL;
+    }
 
-    if (m_aGradient != NULL) delete m_aGradient;
+    if (m_aDelta) {
+        delete m_aDelta;
+        m_aDelta = NULL;
+    }
 
-    if (m_aDelta != NULL) delete m_aDelta;
+    if (m_apOutput) {
+        delete[] m_apOutput;
+        m_apOutput = NULL;
+    }
 
-    delete[] m_apOutputOperator;
-    delete[] m_apInputOperator;
-    // delete m_aOptimizer;
+    if (m_apInput) {
+        delete[] m_apInput;
+        m_apInput = NULL;
+    }
 }
-
-// int Operator<DTYPE>::DeleteInputOperator() {
-//// Postorder : like ForwardPropagate
-// for (int i = 0; i < m_InputDegree; i++) {
-// m_apInputOperator[i]->IncreaseCurrentOutputDegree();
-//
-// if (m_apInputOperator[i]->GetCurrentOutputDegree() == 1) {
-// m_apInputOperator[i]->DeleteInputOperator();
-// }
-//
-// if (m_apInputOperator[i]->GetOutputDegree() == m_apInputOperator[i]->GetCurrentOutputDegree()) {
-//// std::cout << '\n' << m_apInputOperator[i]->GetName() << '\n' << std::endl;
-// delete m_apInputOperator[i];
-// }
-// }
-//
-// return 1;
-// }
-
-// ===========================================================================================
 
 // Add Graph Edge
-template<typename DTYPE>
-int Operator<DTYPE>::_AddInputEdge(Operator<DTYPE> *pInput) {
-    if (m_InputDegree != 0) {
-        Operator<DTYPE> **temp = new Operator<DTYPE> *[m_InputDegree + 1];
-        std::copy(m_apInputOperator, m_apInputOperator + m_InputDegree, temp);
+template<typename DTYPE> int Operator<DTYPE>::_AddInputEdge(Operator<DTYPE> *pInput) {
+    Operator<DTYPE> **temp = new Operator<DTYPE> *[m_InputDegree + 1];
 
-        delete[] m_apInputOperator;
+    for (int i = 0; i < m_InputDegree; i++) temp[i] = m_apInput[i];
+    temp[m_InputDegree] = pInput;
 
-        m_apInputOperator = temp;
-    } else {
-        m_apInputOperator = new Operator<DTYPE> *[m_InputDegree + 1];
+    if (m_apInput) {
+        delete[] m_apInput;
+        m_apInput = NULL;
     }
 
-    m_apInputOperator[m_InputDegree] = pInput;
+    m_apInput = temp;
 
     m_InputDegree++;
 
-    return 1;
+    return TRUE;
 }
 
-template<typename DTYPE>
-int Operator<DTYPE>::_AddOutputEdge(Operator<DTYPE> *pOutput) {
-    if (m_OutputDegree != 0) {
-        Operator<DTYPE> **temp = new Operator<DTYPE> *[m_OutputDegree + 1];
-        std::copy(m_apOutputOperator, m_apOutputOperator + m_OutputDegree, temp);
+template<typename DTYPE> int Operator<DTYPE>::_AddOutputEdge(Operator<DTYPE> *pOutput) {
+    Operator<DTYPE> **temp = new Operator<DTYPE> *[m_OutputDegree + 1];
 
-        delete[] m_apOutputOperator;
+    for (int i = 0; i < m_OutputDegree; i++) temp[i] = m_apOutput[i];
+    temp[m_OutputDegree] = pOutput;
 
-        m_apOutputOperator = temp;
-    } else {
-        m_apOutputOperator = new Operator<DTYPE> *[m_OutputDegree + 1];
+    if (m_apOutput) {
+        delete[] m_apOutput;
+        m_apOutput = NULL;
     }
 
-    m_apOutputOperator[m_OutputDegree] = pOutput;
+    m_apOutput = temp;
 
     m_OutputDegree++;
 
-    return 1;
+    return TRUE;
 }
 
-template<typename DTYPE>
-int Operator<DTYPE>::AddEdgebetweenOperators(Operator<DTYPE> *pInput) {
-    // 양방향 Edge 생성
+template<typename DTYPE> int Operator<DTYPE>::AddEdgebetweenOperators(Operator<DTYPE> *pInput) {
     this->_AddInputEdge(pInput);
     pInput->_AddOutputEdge(this);
 
-    return 1;
+    return TRUE;
+}
+
+template<typename DTYPE> void Operator<DTYPE>::IncreaseCurrentOutputDegree() {
+    m_currentOutputDegree++;
+}
+
+template<typename DTYPE> void Operator<DTYPE>::IncreaseCurrentInputDegree() {
+    m_currentInputDegree++;
+}
+
+template<typename DTYPE> Tensor<DTYPE> *Operator<DTYPE>::GetResult() const {
+    return m_aResult;
+}
+
+template<typename DTYPE> Tensor<DTYPE> *Operator<DTYPE>::GetGradient() const {
+    return m_aGradient;
+}
+
+template<typename DTYPE> Tensor<DTYPE> *Operator<DTYPE>::GetDelta() const {
+    return m_aDelta;
+}
+
+template<typename DTYPE> Operator<DTYPE> **Operator<DTYPE>::GetOutput() const {
+    return m_apOutput;
+}
+
+template<typename DTYPE> Operator<DTYPE> **Operator<DTYPE>::GetInput() const {
+    return m_apInput;
+}
+
+template<typename DTYPE> int Operator<DTYPE>::GetOutputDegree() const {
+    return m_OutputDegree;
+}
+
+template<typename DTYPE> int Operator<DTYPE>::GetInputDegree() const {
+    return m_InputDegree;
+}
+
+template<typename DTYPE> int Operator<DTYPE>::GetCurrentOutputDegree() const {
+    return m_currentOutputDegree;
+}
+
+template<typename DTYPE> int Operator<DTYPE>::GetCurrentInputDegree() const {
+    return m_currentInputDegree;
+}
+
+template<typename DTYPE> std::string Operator<DTYPE>::GetName() const {
+    return m_name;
 }
 
 // ===========================================================================================
 
-/* BFS로 다시 구현할 필요 있음 */
-
-// int Operator<DTYPE>::ForwardPropagate(){
-//// Preorder
-// this->ComputeForwardPropagate();
-//
-//// value 조정
-// for (int i = 0; i < m_OutputDegree; i++) {
-// if (m_apOutputOperator[i] != NULL) m_apOutputOperator[i]->IncreaseCurrentInputDegree();
-// }
-// m_currentInputDegree = 0;
-//
-// for (int i = 0; i < m_OutputDegree; i++) {
-// if (m_apOutputOperator[i]->GetInputDegree() == m_apOutputOperator[i]->GetCurrentInputDegree()) {
-// m_apOutputOperator[i]->ForwardPropagate();
-// }
-// }
-// return 1;
-// }
-
-template<typename DTYPE>
-int Operator<DTYPE>::ForwardPropagate() {
-    // 알고리즘 잘 이해하기
-    // BFS로 나중에는 바꿀 것
+template<typename DTYPE> int Operator<DTYPE>::ForwardPropagate() {
     if (m_InputDegree == m_currentInputDegree) {
         this->ComputeForwardPropagate();
 
         for (int o = 0; o < m_OutputDegree; o++) {
-            m_apOutputOperator[o]->IncreaseCurrentInputDegree();
+            m_apOutput[o]->IncreaseCurrentInputDegree();
         }
         m_currentInputDegree = 0;
     } else {
         for (int i = 0; i < m_InputDegree; i++) {
-            m_apInputOperator[i]->ForwardPropagate();
+            m_apInput[i]->ForwardPropagate();
 
             if (m_InputDegree == m_currentInputDegree) {
                 this->ComputeForwardPropagate();
 
                 for (int o = 0; o < m_OutputDegree; o++) {
-                    m_apOutputOperator[o]->IncreaseCurrentInputDegree();
+                    m_apOutput[o]->IncreaseCurrentInputDegree();
                 }
                 m_currentInputDegree = 0;
             }
         }
     }
 
-    return 1;
+    return TRUE;
 }
 
-template<typename DTYPE>
-int Operator<DTYPE>::BackPropagate() {
+template<typename DTYPE> int Operator<DTYPE>::ComputeForwardPropagate() {
+    return TRUE;
+}
+
+template<typename DTYPE> int Operator<DTYPE>::BackPropagate() {
     this->ComputeBackPropagate();
 
     // value 조정
     for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i] != NULL) m_apInputOperator[i]->IncreaseCurrentOutputDegree();
+        if (m_apInput[i] != NULL) m_apInput[i]->IncreaseCurrentOutputDegree();
     }
     m_currentOutputDegree = 0;
 
-    // Back propagation을 하다가 base operator가 나오지 않으면, 실행되지 않은 Placeholder가 있지는 않은지 확인해볼 것
     for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i]->GetOutputDegree() == m_apInputOperator[i]->GetCurrentOutputDegree()) {
-            m_apInputOperator[i]->BackPropagate();
+        if (m_apInput[i]->GetOutputDegree() == m_apInput[i]->GetCurrentOutputDegree()) {
+            m_apInput[i]->BackPropagate();
         }
     }
-    return 1;
+    return TRUE;
 }
 
-// ===========================================================================================
-
-template<typename DTYPE>
-int Operator<DTYPE>::ComputeForwardPropagate() {
-    // std::cout << m_name << " : ComputeForwardPropagate()" << '\n';
-
-    return 1;
+template<typename DTYPE> int Operator<DTYPE>::ComputeBackPropagate() {
+    return TRUE;
 }
 
-template<typename DTYPE>
-int Operator<DTYPE>::ComputeBackPropagate() {
-    // std::cout << m_name << " : ComputeBackPropagate()" << '\n';
+int main(int argc, char const *argv[]) {
+    Operator<int> *temp1 = new Operator<int>(new Tensor<int>(1, 100, 1, 28, 28), "temp1");
+    Operator<int> *temp2 = new Operator<int>(temp1, "temp2");
+    Operator<int> *temp3 = new Operator<int>(temp1, temp2, "temp3");
 
-    return 1;
+    std::cout << temp3->GetInput()[0]->GetName() << '\n';
+    std::cout << temp3->GetInput()[1]->GetName() << '\n';
+
+    delete temp1;
+    delete temp2;
+    delete temp3;
+
+    return 0;
 }
-
-// ===========================================================================================
-
-template<typename DTYPE>
-void Operator<DTYPE>::PrintGraph(int depth) {
-    std::cout << this->GetName() << '\n';
-
-    // value 조정
-    for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i] != NULL) m_apInputOperator[i]->IncreaseCurrentOutputDegree();
-    }
-    m_currentOutputDegree = 0;
-
-    // Back propagation을 하다가 base operator가 나오지 않으면, 실행되지 않은 Placeholder가 있지는 않은지 확인해볼 것
-    for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i]->GetOutputDegree() == m_apInputOperator[i]->GetCurrentOutputDegree()) {
-            m_apInputOperator[i]->PrintGraph(depth);
-        }
-    }
-}
-
-template<typename DTYPE>
-void Operator<DTYPE>::PrintData(int forceprint) {
-    std::cout << "\n\n" << this->GetName() << ": PrintData()" << '\n';
-
-    if (m_aOutput != NULL) this->PrintOutput(forceprint);
-
-    if (m_aDelta != NULL) this->PrintDelta(forceprint);
-
-    if (m_aGradient != NULL) this->PrintGradient(forceprint);
-
-    std::cout << this->GetName() << ": ~PrintData() \n\n";
-
-    // value 조정
-    for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i] != NULL) m_apInputOperator[i]->IncreaseCurrentOutputDegree();
-    }
-    m_currentOutputDegree = 0;
-
-    // Back propagation을 하다가 base operator가 나오지 않으면, 실행되지 않은 Placeholder가 있지는 않은지 확인해볼 것
-    for (int i = 0; i < m_InputDegree; i++) {
-        if (m_apInputOperator[i]->GetOutputDegree() == m_apInputOperator[i]->GetCurrentOutputDegree()) {
-            m_apInputOperator[i]->PrintData();
-        }
-    }
-}
-
-// ===========================================================================================
-
-// template <typename DTYPE>
-// Operator<DTYPE> * Operator<DTYPE>::CheckEndOperator() {
-//// recursively
-// if (m_OutputDegree == 0) {
-// return this;
-// } else {
-//// 추후에는 모든 Operator의 Output을 확인하도록 한다.
-// return m_apOutputOperator[0]->CheckEndOperator();
-// }
-// }
--- Header/Operator.h
+++ Header/Operator.h
@@ -1,251 +1,66 @@
 #ifndef OPERATOR_H_
 #define OPERATOR_H_
 
-#include "MetaParameter.h"
-#include "Optimizer//GradientDescentOptimizer.h"
+// #include "MetaParameter.h"
+// #include "Optimizer//GradientDescentOptimizer.h"
+#include "Tensor.h"
 
 template<typename DTYPE>
 class Operator {
 private:
-    Tensor<DTYPE> *m_aOutput = NULL;
+    Tensor<DTYPE> *m_aResult;
+    Tensor<DTYPE> *m_aGradient;
+    Tensor<DTYPE> *m_aDelta;
 
-    // Training 과정을 공부한 후 다시 확인해야 할 부분
-    // Gradient의 경우는 자신의 Output Operator에서 계산해서 이미 넘겨준 상태 (계산 과정 잘 생각해보기)
-    Tensor<DTYPE> *m_aGradient = NULL;
-    Tensor<DTYPE> *m_aDelta    = NULL;
+    Operator<DTYPE> **m_apOutput;
+    Operator<DTYPE> **m_apInput;
 
-    // for Linked List
-    // Pointer array를 만들기 위한 공간으로 Alloc할 때 공간을 동적할당한다.
-    Operator<DTYPE> **m_apOutputOperator = NULL;
-    Operator<DTYPE> **m_apInputOperator  = NULL;
+    int m_OutputDegree;
+    int m_InputDegree;
+    int m_currentOutputDegree;
+    int m_currentInputDegree;
 
-    int m_OutputDegree = 0;
-    int m_InputDegree  = 0;
-
-    int m_currentOutputDegree = 0;
-    int m_currentInputDegree  = 0;
-
-    // identifier
-    std::string m_name = "NO NAME";
-
-    int m_Trainable = 0;
-    // Private Operator
-
-private:
-    int _AddInputEdge(Operator<DTYPE> *pInput);
-    int _AddOutputEdge(Operator<DTYPE> *pOutput);
+    std::string m_name;
 
 public:
-    Operator() {
-        std::cout << "Operator<DTYPE>::Operator()" << '\n';
-    }
+    Operator(std::string pName = "NO NAME");
+    Operator(Tensor<DTYPE> *pTensor, std::string pName = "NO NAME");
+    Operator(Operator<DTYPE> *pInput, std::string pName = "NO NAME");
+    Operator(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1, std::string pName = "NO NAME");
+    virtual ~Operator();
 
-    Operator(std::string pName) {
-        std::cout << "Operator<DTYPE>::Operator(std::string)" << '\n';
-        m_name = pName;
-    }
+    virtual int       Alloc(Tensor<DTYPE> *pTensor);
+    virtual int       Alloc(int numInput, ...);
+    virtual void      Delete();
 
-    // ===========================================================================================
+    int               _AddInputEdge(Operator<DTYPE> *pInput);
+    int               _AddOutputEdge(Operator<DTYPE> *pOutput);
+    int               AddEdgebetweenOperators(Operator<DTYPE> *pInput);
 
-    Operator(Tensor<DTYPE> *pTensor) {
-        std::cout << "Operator<DTYPE>::Operator(Tensor<DTYPE> *, std::string pName)" << '\n';
-        this->Alloc(pTensor);
-    }
+    void              SetResult(Tensor<DTYPE> *pTensor);
+    void              SetGradient(Tensor<DTYPE> *pTensor);
+    void              SetDelta(Tensor<DTYPE> *pTensor);
+    void              IncreaseCurrentOutputDegree();
+    void              IncreaseCurrentInputDegree();
 
-    Operator(Tensor<DTYPE> *pTensor, std::string pName) : Operator(pName) {
-        std::cout << "Operator<DTYPE>::Operator(Tensor<DTYPE> *, std::string pName)" << '\n';
-        this->Alloc(pTensor);
-    }
+    Tensor<DTYPE>   * GetResult() const;
+    Tensor<DTYPE>   * GetGradient() const;
+    Tensor<DTYPE>   * GetDelta() const;
+    Operator<DTYPE>** GetInput() const;
+    Operator<DTYPE>** GetOutput() const;
+    int               GetOutputDegree() const;
+    int               GetInputDegree() const;
+    int               GetCurrentOutputDegree() const;
+    int               GetCurrentInputDegree() const;
+    std::string       GetName() const;
 
-    // ===========================================================================================
+    // For Propagate
+    int               ForwardPropagate();
+    virtual int       ComputeForwardPropagate();
 
-    Operator(Operator<DTYPE> *pInput) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *)" << '\n';
-        this->Alloc(pInput);
-    }
-
-    Operator(Operator<DTYPE> *pInput, std::string pName) : Operator(pName) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *, std::string)" << '\n';
-        this->Alloc(pInput);
-    }
-
-    // ===========================================================================================
-
-    Operator(Operator<DTYPE> *pInput, MetaParameter<DTYPE> *pParam) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *, MetaParameter<DTYPE> *)" << '\n';
-        this->Alloc(pInput);
-    }
-
-    Operator(Operator<DTYPE> *pInput, MetaParameter<DTYPE> *pParam, std::string pName) : Operator(pName) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *, MetaParameter<DTYPE> *, std::string)" << '\n';
-        this->Alloc(pInput);
-    }
-
-    // ===========================================================================================
-
-    Operator(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
-        this->Alloc(pInput0, pInput1);
-    }
-
-    Operator(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1, std::string pName) : Operator(pName) {
-        std::cout << "Operator<DTYPE>::Operator(Operator<DTYPE> *, Operator<DTYPE> *, std::string)" << '\n';
-        this->Alloc(pInput0, pInput1);
-    }
-
-    // ===========================================================================================
-
-    virtual ~Operator() {
-        std::cout << "Operator<DTYPE>::~Operator()" << '\n';
-
-        // parent class Delete
-        this->Delete();
-
-        // 자식 클래스에서는 새롭게 만들어지는 MetaParameter에 관한 delete를 만들어주어야 한다
-    }
-
-    // ===========================================================================================
-
-    // 추후 Private으로 옮길 의향 있음
-    virtual int Alloc(Tensor<DTYPE> *pTensor);
-    virtual int Alloc(Operator<DTYPE> *pInput);
-    virtual int Alloc(Operator<DTYPE> *pInput0, Operator<DTYPE> *pInput1);
-    virtual int Alloc(MetaParameter<DTYPE> *pParam = NULL);
-
-    int         AllocOptimizer(Optimizer<DTYPE> *pOptimizer);
-
-    virtual void Delete();
-
-    // int         DeleteInputOperator();
-
-    // ===========================================================================================
-
-    int AddEdgebetweenOperators(Operator<DTYPE> *pInput);
-
-    // ===========================================================================================
-
-    //// Setter
-    // Gradient 부분은 Trainable한 부분에서만 만들기에 NULL로 초기화할 가능성이 생길 것으로 보인다.
-    void SetOutput(Tensor<DTYPE> *pTensor) {
-        m_aOutput = pTensor;
-    }
-
-    void FeedOutput(Tensor<DTYPE> *pTensor) {
-        m_aOutput = pTensor;
-    }
-
-    void SetGradient(Tensor<DTYPE> *pTensor) {
-        m_aGradient = pTensor;
-    }
-
-    void SetDelta(Tensor<DTYPE> *pTensor) {
-        m_aDelta = pTensor;
-    }
-
-    void SetTrainable(int pTrainable) {
-        m_Trainable = pTrainable;
-    }
-
-    // ===========================================================================================
-
-    void IncreaseCurrentOutputDegree() {
-        m_currentOutputDegree++;
-    }
-
-    void IncreaseCurrentInputDegree() {
-        m_currentInputDegree++;
-    }
-
-    // ===========================================================================================
-
-    //
-    //// Getter (파생 클래스에서 사용합니다.)
-
-    Tensor<DTYPE>* GetOutput() const {
-        return m_aOutput;
-    }
-
-    Tensor<DTYPE>* GetGradient() const {
-        return m_aGradient;
-    }
-
-    Tensor<DTYPE>* GetDelta() const {
-        return m_aDelta;
-    }
-
-    Operator<DTYPE>** GetInputOperator() const {
-        return m_apInputOperator;
-    }
-
-    Operator<DTYPE>** GetOutputOperator() const {
-        return m_apOutputOperator;
-    }
-
-    int GetOutputDegree() const {
-        return m_OutputDegree;
-    }
-
-    int GetInputDegree() const {
-        return m_InputDegree;
-    }
-
-    int GetCurrentOutputDegree() const {
-        return m_currentOutputDegree;
-    }
-
-    int GetCurrentInputDegree() const {
-        return m_currentInputDegree;
-    }
-
-    std::string GetName() const{
-        return m_name;
-    }
-
-    int GetTrainable() const{
-        return m_Trainable;
-    }
-
-    // ===========================================================================================
-
-    // Propagate
-    int         ForwardPropagate();        // ForwardPropagate 진행 방향 및 순서
-    virtual int ComputeForwardPropagate();  // Compute to (추후 interface 형태로 수정 예정)
-
-    // BackPropagate
-    int         BackPropagate(); // BackPropagate 진행 방향 및 순서
-    virtual int ComputeBackPropagate();  // compute delta and detabar(if we need to) (추후 interface 형태로 수정 예정)
-
-
-    // ===========================================================================================
-
-    void PrintGraph(int depth = 0);
-
-    void PrintData(int forceprint = 0);
-
-    void PrintOutput(int forceprint = 0) {
-        std::cout << this->GetName() << ": Shape of Output" << '\n';
-        this->GetOutput()->PrintShape();
-        std::cout << this->GetName() << ": Value of Output" << '\n';
-        this->GetOutput()->PrintData(forceprint);
-    }
-
-    void PrintDelta(int forceprint = 0) {
-        std::cout << this->GetName() << ": Shape of Delta" << '\n';
-        this->GetDelta()->PrintShape();
-        std::cout << this->GetName() << ": Value of Delta" << '\n';
-        this->GetDelta()->PrintData(forceprint);
-    }
-
-    void PrintGradient(int forceprint = 0) {
-        std::cout << this->GetName() << ": Shape of Gradient" << '\n';
-        this->GetGradient()->PrintShape();
-        std::cout << this->GetName() << ": Value of Gradient" << '\n';
-        this->GetGradient()->PrintData(forceprint);
-    }
-
-    // ===========================================================================================
-
-    // Operator<DTYPE>* CheckEndOperator();
+    // For BackPropagate
+    int               BackPropagate();
+    virtual int       ComputeBackPropagate();
 };
 
 #endif  // OPERATOR_H_
