901356312732b47e476aa2320cb86ec5b8232174
chuniee0126 <chunie0126@gmail.com>
2018-05-18 20:27:07 +0900

    mod : Add Operator not broad cast - cmpark

--- Header/Operator/Add.h
+++ Header/Operator/Add.h
@@ -73,7 +73,224 @@ public:
     }
 };
 
-template<typename DTYPE> class Add : public Operator<DTYPE>{
+
+template<typename DTYPE> class AddColWise : public Operator<DTYPE>{
+private:
+    Shape *m_pInputTenShape;
+    Shape *m_pBiasTenShape;
+
+    int m_timesize;
+    int m_batchsize;
+    int m_channelsize;
+    int m_rowsize;
+    int m_colsize;
+
+public:
+    AddColWise(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias, std::string pName) : Operator<DTYPE>(pInput, pBias, pName) {
+        #if __DEBUG__
+        std::cout << "AddColWise::AddColWise(Operator<DTYPE> *, Operator<DTYPE> *, std::string)" << '\n';
+        #endif  // __DEBUG__
+        this->Alloc(pInput, pBias);
+    }
+
+    ~AddColWise() {
+        #if __DEBUG__
+        std::cout << "AddColWise::~AddColWise()" << '\n';
+        #endif  // __DEBUG__
+    }
+
+    int Alloc(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias) {
+        #if __DEBUG__
+        std::cout << "AddColWise::Alloc(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
+        #endif  // __DEBUG__
+
+        m_pInputTenShape = pInput->GetResult()->GetShape();
+        m_pBiasTenShape  = pBias->GetResult()->GetShape();
+
+        m_timesize    = (*m_pInputTenShape)[0];
+        m_batchsize   = (*m_pInputTenShape)[1];
+        m_channelsize = (*m_pInputTenShape)[2];
+        m_rowsize     = (*m_pInputTenShape)[3];
+        m_colsize     = (*m_pInputTenShape)[4];
+
+        #if __DEBUG__
+
+        if ((*m_pBiasTenShape)[0] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[1] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[2] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[3] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+        #endif  // __DEBUG__
+
+        this->SetResult(new Tensor<DTYPE>(m_timesize, m_batchsize, m_channelsize, m_rowsize, m_colsize));
+
+        this->SetGradient(new Tensor<DTYPE>(m_timesize, m_batchsize, m_channelsize, m_rowsize, m_colsize));
+
+        return TRUE;
+    }
+
+    int ForwardPropagate() {
+        Container<Operator<DTYPE> *> *input_contatiner = this->GetInputContainer();
+
+        Tensor<DTYPE> *input  = (*input_contatiner)[0]->GetResult();
+        Tensor<DTYPE> *bias   = (*input_contatiner)[1]->GetResult();
+        Tensor<DTYPE> *result = this->GetResult();
+
+        for (int m_ti = 0; m_ti < m_timesize; m_ti++) {
+            for (int m_ba = 0; m_ba < m_batchsize; m_ba++) {
+                for (int m_ch = 0; m_ch < m_channelsize; m_ch++) {
+                    for (int m_ro = 0; m_ro < m_rowsize; m_ro++) {
+                        for (int m_co = 0; m_co < m_colsize; m_co++) {
+                            (*result)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                = (*input)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                  + (*bias)[m_co];
+                        }
+                    }
+                }
+            }
+        }
+
+        return TRUE;
+    }
+
+    int BackPropagate() {
+        Container<Operator<DTYPE> *> *input_contatiner = this->GetInputContainer();
+
+        Tensor<DTYPE> *input_grad = (*input_contatiner)[0]->GetGradient();
+        Tensor<DTYPE> *bias_grad  = (*input_contatiner)[1]->GetGradient();
+        Tensor<DTYPE> *this_grad  = this->GetGradient();
+
+        for (int m_ti = 0; m_ti < m_timesize; m_ti++) {
+            for (int m_ba = 0; m_ba < m_batchsize; m_ba++) {
+                for (int m_ch = 0; m_ch < m_channelsize; m_ch++) {
+                    for (int m_ro = 0; m_ro < m_rowsize; m_ro++) {
+                        for (int m_co = 0; m_co < m_colsize; m_co++) {
+                            (*input_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                += (*this_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)];
+
+                            (*bias_grad)[m_co]
+                                += (*this_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)];
+                        }
+                    }
+                }
+            }
+        }
+
+        return TRUE;
+    }
+};
+
+template<typename DTYPE> class AddChannelWise : public Operator<DTYPE>{
+private:
+    Shape *m_pInputTenShape;
+    Shape *m_pBiasTenShape;
+
+    int m_timesize;
+    int m_batchsize;
+    int m_channelsize;
+    int m_rowsize;
+    int m_colsize;
+
+public:
+    AddChannelWise(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias, std::string pName) : Operator<DTYPE>(pInput, pBias, pName) {
+        #if __DEBUG__
+        std::cout << "AddChannelWise::AddChannelWise(Operator<DTYPE> *, Operator<DTYPE> *, std::string)" << '\n';
+        #endif  // __DEBUG__
+        this->Alloc(pInput, pBias);
+    }
+
+    ~AddChannelWise() {
+        #if __DEBUG__
+        std::cout << "AddChannelWise::~AddChannelWise()" << '\n';
+        #endif  // __DEBUG__
+    }
+
+    int Alloc(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias) {
+        #if __DEBUG__
+        std::cout << "AddColWise::Alloc(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
+        #endif  // __DEBUG__
+
+        m_pInputTenShape = pInput->GetResult()->GetShape();
+        m_pBiasTenShape  = pBias->GetResult()->GetShape();
+
+        m_timesize    = (*m_pInputTenShape)[0];
+        m_batchsize   = (*m_pInputTenShape)[1];
+        m_channelsize = (*m_pInputTenShape)[2];
+        m_rowsize     = (*m_pInputTenShape)[3];
+        m_colsize     = (*m_pInputTenShape)[4];
+
+        #if __DEBUG__
+
+        if ((*m_pBiasTenShape)[0] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[1] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[3] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+
+        if ((*m_pBiasTenShape)[4] != 1) printf("Receive invalid bias shape in %s (%s %d), cannot handling\n", __FUNCTION__, __FILE__, __LINE__);
+        #endif  // __DEBUG__
+
+        this->SetResult(new Tensor<DTYPE>(m_timesize, m_batchsize, m_channelsize, m_rowsize, m_colsize));
+
+        this->SetGradient(new Tensor<DTYPE>(m_timesize, m_batchsize, m_channelsize, m_rowsize, m_colsize));
+
+        return TRUE;
+    }
+
+    int ForwardPropagate() {
+        Container<Operator<DTYPE> *> *input_contatiner = this->GetInputContainer();
+
+        Tensor<DTYPE> *input  = (*input_contatiner)[0]->GetResult();
+        Tensor<DTYPE> *bias   = (*input_contatiner)[1]->GetResult();
+        Tensor<DTYPE> *result = this->GetResult();
+
+        for (int m_ti = 0; m_ti < m_timesize; m_ti++) {
+            for (int m_ba = 0; m_ba < m_batchsize; m_ba++) {
+                for (int m_ch = 0; m_ch < m_channelsize; m_ch++) {
+                    for (int m_ro = 0; m_ro < m_rowsize; m_ro++) {
+                        for (int m_co = 0; m_co < m_colsize; m_co++) {
+                            (*result)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                = (*input)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                  + (*bias)[m_ch];
+                        }
+                    }
+                }
+            }
+        }
+
+        return TRUE;
+    }
+
+    int BackPropagate() {
+        Container<Operator<DTYPE> *> *input_contatiner = this->GetInputContainer();
+
+        Tensor<DTYPE> *input_grad = (*input_contatiner)[0]->GetGradient();
+        Tensor<DTYPE> *bias_grad  = (*input_contatiner)[1]->GetGradient();
+        Tensor<DTYPE> *this_grad  = this->GetGradient();
+
+        for (int m_ti = 0; m_ti < m_timesize; m_ti++) {
+            for (int m_ba = 0; m_ba < m_batchsize; m_ba++) {
+                for (int m_ch = 0; m_ch < m_channelsize; m_ch++) {
+                    for (int m_ro = 0; m_ro < m_rowsize; m_ro++) {
+                        for (int m_co = 0; m_co < m_colsize; m_co++) {
+                            (*input_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)]
+                                += (*this_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)];
+
+                            (*bias_grad)[m_ch]
+                                += (*this_grad)[Index5D(m_pInputTenShape, m_ti, m_ba, m_ch, m_ro, m_co)];
+                        }
+                    }
+                }
+            }
+        }
+
+        return TRUE;
+    }
+};
+
+template<typename DTYPE> class AddBroadcast : public Operator<DTYPE>{
 private:
     Shape *m_pInputTenShape;
     Shape *m_pBiasTenShape;
@@ -99,22 +316,22 @@ private:
     int m_zero;
 
 public:
-    Add(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias, std::string pName) : Operator<DTYPE>(pInput, pBias, pName) {
+    AddBroadcast(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias, std::string pName) : Operator<DTYPE>(pInput, pBias, pName) {
         #if __DEBUG__
-        std::cout << "Add::Add(Operator<DTYPE> *, Operator<DTYPE> *, std::string)" << '\n';
+        std::cout << "AddBroadcast::AddBroadcast(Operator<DTYPE> *, Operator<DTYPE> *, std::string)" << '\n';
         #endif  // __DEBUG__
         this->Alloc(pInput, pBias);
     }
 
-    ~Add() {
+    ~AddBroadcast() {
         #if __DEBUG__
-        std::cout << "Add::~Add()" << '\n';
+        std::cout << "AddBroadcast::~AddBroadcast()" << '\n';
         #endif  // __DEBUG__
     }
 
     int Alloc(Operator<DTYPE> *pInput, Operator<DTYPE> *pBias) {
         #if __DEBUG__
-        std::cout << "Add::Alloc(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
+        std::cout << "AddBroadcast::Alloc(Operator<DTYPE> *, Operator<DTYPE> *)" << '\n';
         #endif  // __DEBUG__
 
         m_pInputTenShape = pInput->GetResult()->GetShape();
@@ -261,5 +478,6 @@ public:
         return TRUE;
     }
 };
+
 
 #endif  // ADD_H_
