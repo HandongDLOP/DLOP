d55679f7fcf87839f79849c0fd34e6b027e404a7
chuniee0126 <chunie0126@gmail.com>
2018-05-19 03:56:53 +0900

    mod : propagate algorithm for thread - cmpark

--- Header/LossFunction/MSE.h
+++ Header/LossFunction/MSE.h
@@ -48,8 +48,6 @@ public:
         Tensor<DTYPE> *label    = this->GetLabel()->GetResult();
         Tensor<DTYPE> *result   = this->GetResult();
         Tensor<DTYPE> *gradient = this->GetGradient();
-        // result->Reset();
-        // gradient->Reset();
 
         int timesize  = input->GetTimeSize();
         int batchsize = input->GetBatchSize();
@@ -64,25 +62,18 @@ public:
 
         int i = 0;
 
-        for (int ti = 0; ti < timesize; ti++) {
-            for (int ba = 0; ba < batchsize; ba++) {
-                i = ti * batchsize + ba;
+        int ti          = 0;
+        int numOfThread = this->GetNumOfThread();
 
-                for (int j = 0; j < capacity; j++) {
-                    index              = i * capacity + j;
-                    (*result)[i]      += Error((*input)[index], (*label)[index]);
-                    (*gradient)[index] = ((*input)[index] - (*label)[index]);
-                }
+        for (int ba = pThreadNum; ba < batchsize; ba += numOfThread) {
+            i = ti * batchsize + ba;
+
+            for (int j = 0; j < capacity; j++) {
+                index              = i * capacity + j;
+                (*result)[i]      += Error((*input)[index], (*label)[index]);
+                (*gradient)[index] = ((*input)[index] - (*label)[index]);
             }
         }
-
-        // for (int i = 0; i < count; i++) {
-        // for (int j = 0; j < capacity; j++) {
-        // index              = i * capacity + j;
-        // (*result)[i]      += Error((*input)[index], (*label)[index]);
-        // (*gradient)[index] = ((*input)[index] - (*label)[index]);
-        // }
-        // }
 
         return result;
     }
@@ -103,87 +94,7 @@ public:
         int index = 0;
         int i     = 0;
 
-        for (int ti = 0; ti < timesize; ti++) {
-            for (int ba = 0; ba < batchsize; ba++) {
-                i = ti * batchsize + ba;
-
-                for (int j = 0; j < capacity; j++) {
-                    index                  = i * capacity + j;
-                    (*input_delta)[index] += (*gradient)[index] / batchsize;
-                }
-            }
-        }
-        // int batchsize = gradient->GetBatchSize();
-        //
-        // int capacity = input_delta->GetCapacity();
-
-        // for (int i = 0; i < capacity; i++) {
-        // (*input_delta)[i] += (*gradient)[i] / batchsize;
-        // }
-
-        return NULL;
-    }
-
-    virtual Tensor<DTYPE>* ForwardPropagate(int pTime, int pThreadNum) {
-        Tensor<DTYPE> *input    = this->GetTensor();
-        Tensor<DTYPE> *label    = this->GetLabel()->GetResult();
-        Tensor<DTYPE> *result   = this->GetResult();
-        Tensor<DTYPE> *gradient = this->GetGradient();
-
-        int timesize  = input->GetTimeSize();
-        int batchsize = input->GetBatchSize();
-        int count     = timesize * batchsize;
-
-        int channelsize = input->GetChannelSize();
-        int rowsize     = input->GetRowSize();
-        int colsize     = input->GetColSize();
-        int capacity    = channelsize * rowsize * colsize;
-
-        int index = 0;
-
-        int i = 0;
-
-        int ti          = pTime;
-        int numOfThread = this->GetNumOfThread();
-
-        for (int ba = pThreadNum; ba < batchsize; ba += numOfThread) {
-            i = ti * batchsize + ba;
-
-            for (int j = 0; j < capacity; j++) {
-                index              = i * capacity + j;
-                (*result)[i]      += Error((*input)[index], (*label)[index]);
-                (*gradient)[index] = ((*input)[index] - (*label)[index]);
-            }
-        }
-
-        // for (int i = 0; i < count; i++) {
-        // for (int j = 0; j < capacity; j++) {
-        // index              = i * capacity + j;
-        // (*result)[i]      += Error((*input)[index], (*label)[index]);
-        // (*gradient)[index] = ((*input)[index] - (*label)[index]);
-        // }
-        // }
-
-        return result;
-    }
-
-    virtual Tensor<DTYPE>* BackPropagate(int pTime, int pThreadNum) {
-        Tensor<DTYPE> *gradient    = this->GetGradient();
-        Tensor<DTYPE> *input_delta = this->GetOperator()->GetDelta();
-
-        int timesize  = gradient->GetTimeSize();
-        int batchsize = gradient->GetBatchSize();
-        int count     = timesize * batchsize;
-
-        int channelsize = gradient->GetChannelSize();
-        int rowsize     = gradient->GetRowSize();
-        int colsize     = gradient->GetColSize();
-        int capacity    = channelsize * rowsize * colsize;
-
-        int index = 0;
-        int i     = 0;
-
-        int ti          = pTime;
+        int ti          = 0;
         int numOfThread = this->GetNumOfThread();
 
         for (int ba = pThreadNum; ba < batchsize; ba += numOfThread) {
@@ -194,14 +105,6 @@ public:
                 (*input_delta)[index] += (*gradient)[index] / batchsize;
             }
         }
-
-        // int batchsize = gradient->GetBatchSize();
-        //
-        // int capacity = input_delta->GetCapacity();
-
-        // for (int i = 0; i < capacity; i++) {
-        // (*input_delta)[i] += (*gradient)[i] / batchsize;
-        // }
 
         return NULL;
     }
--- Header/Operator/Sigmoid.h
+++ Header/Operator/Sigmoid.h
@@ -47,65 +47,7 @@ public:
 
         Shape *resultTenShape = result->GetShape();
 
-        for (int ti = 0; ti < timesize; ti++) {
-            for (int ba = 0; ba < batchsize; ba++) {
-                for (int ch = 0; ch < channelsize; ch++) {
-                    for (int ro = 0; ro < rowsize; ro++) {
-                        for (int co = 0; co < colsize; co++) {
-                            (*result)[Index5D(resultTenShape, ti, ba, ch, ro, co)]
-                                = this->SIGMOID((*input)[Index5D(resultTenShape, ti, ba, ch, ro, co)]);
-                        }
-                    }
-                }
-            }
-        }
-
-        return TRUE;
-    }
-
-    int BackPropagate(int pThreadNum = 0) {
-        Tensor<DTYPE> *result      = this->GetResult();
-        Tensor<DTYPE> *this_delta  = this->GetDelta();
-        Tensor<DTYPE> *input_delta = this->GetInput()[0]->GetDelta();
-
-        int timesize    = result->GetTimeSize();
-        int batchsize   = result->GetBatchSize();
-        int channelsize = result->GetChannelSize();
-        int rowsize     = result->GetRowSize();
-        int colsize     = result->GetColSize();
-
-        Shape *resultTenShape = result->GetShape();
-
-        for (int ti = 0; ti < timesize; ti++) {
-            for (int ba = 0; ba < batchsize; ba++) {
-                for (int ch = 0; ch < channelsize; ch++) {
-                    for (int ro = 0; ro < rowsize; ro++) {
-                        for (int co = 0; co < colsize; co++) {
-                            (*input_delta)[Index5D(resultTenShape, ti, ba, ch, ro, co)]
-                                += (*result)[Index5D(resultTenShape, ti, ba, ch, ro, co)]
-                                   * (1 - (*result)[Index5D(resultTenShape, ti, ba, ch, ro, co)])
-                                   * (*this_delta)[Index5D(resultTenShape, ti, ba, ch, ro, co)];
-                        }
-                    }
-                }
-            }
-        }
-        return TRUE;
-    }
-
-    int ForwardPropagate(int pTime, int pThreadNum) {
-        Tensor<DTYPE> *input  = this->GetInput()[0]->GetResult();
-        Tensor<DTYPE> *result = this->GetResult();
-
-        int timesize    = result->GetTimeSize();
-        int batchsize   = result->GetBatchSize();
-        int channelsize = result->GetChannelSize();
-        int rowsize     = result->GetRowSize();
-        int colsize     = result->GetColSize();
-
-        Shape *resultTenShape = result->GetShape();
-
-        int ti          = pTime;
+        int ti          = 0;
         int numOfThread = this->GetNumOfThread();
 
         for (int ba = pThreadNum; ba < batchsize; ba += numOfThread) {
@@ -123,7 +65,7 @@ public:
         return TRUE;
     }
 
-    int BackPropagate(int pTime, int pThreadNum) {
+    int BackPropagate(int pThreadNum = 0) {
         Tensor<DTYPE> *result      = this->GetResult();
         Tensor<DTYPE> *this_delta  = this->GetDelta();
         Tensor<DTYPE> *input_delta = this->GetInput()[0]->GetDelta();
@@ -136,7 +78,7 @@ public:
 
         Shape *resultTenShape = result->GetShape();
 
-        int ti          = pTime;
+        int ti          = 0;
         int numOfThread = this->GetNumOfThread();
 
         for (int ba = pThreadNum; ba < batchsize; ba += numOfThread) {
