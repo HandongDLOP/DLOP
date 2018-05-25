420504be897da4b6829d5716db85eaf5940f5481
chuniee0126 <chunie0126@gmail.com>
2018-05-25 22:09:56 +0900

    mod : Refactoring Shape Class - cmpark

--- Header/Shape.h
+++ Header/Shape.h
@@ -7,35 +7,44 @@ class Shape {
 private:
     int m_Rank;
     int *m_aDim;
+    Device m_Device;
 
 #if __CUDNN__
     cudnnTensorDescriptor_t m_desc;
 #endif  // if __CUDNN__
 
-public:
-    Shape();
-    Shape(int pSize0, int pSize1, int pSize2, int pSize3, int pSize4);
-    Shape(Shape *pShape);  // Copy Constructor
-    virtual ~Shape();
-
-    int  Alloc();
+private:
     int  Alloc(int pRank, ...);
     int  Alloc(Shape *pShape);
     void Delete();
 
-    void SetRank(int pRank);
+#if __CUDNN__
+    int  AllocOnGPU();
+    void DeleteOnGPU();
+    int  ReShapeOnGPU();
+#endif  // if __CUDNN__
+
+public:
+    Shape(int pSize0, int pSize1, int pSize2, int pSize3, int pSize4);
+    Shape(int pSize0, int pSize1, int pSize2, int pSize3);
+    Shape(int pSize0, int pSize1, int pSize2);
+    Shape(int pSize0, int pSize1);
+    Shape(int pSize0);
+    Shape(Shape *pShape);  // Copy Constructor
+    virtual ~Shape();
+
     int  GetRank();
+    int  GetDim(int pRanknum);
+    int& operator[](int pRanknum);  // operator[] overload
 
     int  ReShape(int pSize0, int pSize1, int pSize2, int pSize3, int pSize4);
-
-    int& operator[](int pRanknum);
+    int  ReShape(int pRank, ...);
 
 
 #if __CUDNN__
     cudnnTensorDescriptor_t& GetDescriptor();
 #endif  // __CUDNN__
 };
-/////////////////////////////////////////////////////////////
 
 std::ostream& operator<<(std::ostream& pOS, Shape *pShape);
 
